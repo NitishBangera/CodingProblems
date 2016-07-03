893cba02cc8cba463e526ff392e59ed224265e5f
Nitish Bangera <bangera.nitish@gmail.com>
2016-07-03 21:13:47 +0530

    Anti clockwise matrix traversal

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,9 +1,6 @@
-/**
-* Clockwise Spiral traversal of a Matrix.
-**/
-
 public class SpiralMatrix {
-  public static void printMatrix(int m, int n) {
+  public static void printMatrixClockwise(int m, int n) {
+    System.out.println("\nClockwise");
     int top = 0;
     int bottom = m - 1;
     int left = 0;
@@ -38,9 +35,44 @@ public class SpiralMatrix {
     }
   }
 
+  public static void printMatrixAntiClockwise(int m, int n) {
+    System.out.println("\nAntiClockwise");
+    int top = 0;
+    int bottom = m - 1;
+    int left = 0;
+    int right = n - 1;
+    while(top <= bottom && left <= right) {
+      // Print leftmost colum.
+      for (int i = top; i <= bottom; i++) {
+        System.out.printf("L(%d, %d) ", i, left);
+      }
+      left++;
+      // Print bottom column.
+      for (int i = left; i <= right; i++) {
+        System.out.printf("B(%d, %d) ", bottom, i);
+      }
+      bottom--;
+      if (left <= right) {
+        // Print rightmost column.
+        for (int i = bottom; i >= top; i--) {
+          System.out.printf("R(%d, %d) ", i, right);
+        }
+        right--;
+      }
+      if (top <= bottom) {
+        // Print top column.
+        for (int i = right; i >= left; i--) {
+          System.out.printf("T(%d, %d) ", top, i);
+        }
+        top++;
+      }
+    }
+  }
+
   public static void main(String... args) {
     int m = Integer.parseInt(args[0]);
     int n = Integer.parseInt(args[1]);
-    printMatrix(m, n);
+    printMatrixClockwise(m, n);
+    printMatrixAntiClockwise(m, n);
   }
 }
