9fbad41321dbd484406ef58e00179367182f4cc1
Nitish Bangera <bangera.nitish@gmail.com>
2020-02-08 15:08:08 +0530

    Minor changes to Node data structure.

--- Node.java
+++ Node.java
@@ -6,32 +6,55 @@ import java.util.Set;
 
 public class Node<T> {
 	private T data;
-	private Set<Node<T>> incoming;
-	private Set<Node<T>> outgoing;
-	
+	private Set<Node<T>> parents;
+	private Set<Node<T>> children;
+
 	public Node(T data) {
 		this.data = data;
-		incoming = new LinkedHashSet<>();
-		outgoing = new LinkedHashSet<>();
+		parents = new LinkedHashSet<>();
+		children = new LinkedHashSet<>();
 	}
-	
+
 	public T getData() {
 		return data;
 	}
-	
-	public Set<Node<T>> getIncoming() {
-		return Collections.unmodifiableSet(incoming);
+
+	public Set<Node<T>> getParents() {
+		return Collections.unmodifiableSet(parents);
 	}
-	
-	public void addIncomingNode(Node<T> node) {
-		incoming.add(node);
+
+	public Set<Node<T>> getChildren() {
+		return Collections.unmodifiableSet(children);
 	}
-	
-	public Set<Node<T>> getOutgoing() {
-		return Collections.unmodifiableSet(outgoing);
+
+	public void addParent(Node<T> parent) {
+		parents.add(parent);
 	}
-	
-	public void addOutgoingNode(Node<T> node) {
-		outgoing.add(node);
+
+	public void addChild(Node<T> child) {
+		child.addParent(this);
+		children.add(child);
+	}
+
+	public void removeParent(Node<T> parent) {
+		parents.remove(parent);
+		parent.removeChild(this);
+	}
+
+	public void removeChild(Node<T> child) {
+		children.remove(child);
+	}
+
+	public boolean isChild(Node<T> child) {
+		return children.contains(child);
+	}
+
+	public boolean isParent(Node<T> parent) {
+		return parents.contains(parent);
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data.toString() + ", parents=" + parents.size() + ", children=" + children.size() + "]";
 	}
 }
